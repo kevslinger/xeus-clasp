9b5f78de498f50adc8ecf777a215c393c770ddc2
Chris Schafmeister <meister@temple.edu>
2020-03-07 01:08:41 -0500

    Updates to connect to clasp

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,10 @@
 #include "xeus-clasp/xinterpreter.hpp"
 #include "xdebugger.hpp"
 
-//#include "clasp/core/foundation.h"
-//#include "clasp/clasp.h"
-//#include "clasp/clbind/clbind.h"
+#include "clasp/clasp.h"
 
 
-std::string extract_filename(int& argc, char* argv[])
+std::string extract_filename(int& argc, const char* argv[])
 {
     std::string res = "";
     for (int i = 0; i < argc; ++i)
@@ -36,65 +34,81 @@ std::string extract_filename(int& argc, char* argv[])
 }
 
 
-int main(int argc, char* argv[])
+extern "C" {
+int start_interpreter()
 {
-  
-  std::string file_name = extract_filename(argc, argv);
-  using interpreter_ptr = std::unique_ptr<xclasp::interpreter>;
-  interpreter_ptr interpreter = interpreter_ptr(new xclasp::interpreter(argc, argv));
+    const char* argv[] = {"xclasp"};
+    int argc = 1;
+    std::string file_name = extract_filename(argc, argv);
+    using interpreter_ptr = std::unique_ptr<xclasp::interpreter>;
+    interpreter_ptr interpreter = interpreter_ptr(new xclasp::interpreter(argc, argv));
     
-  using history_manager_ptr = std::unique_ptr<xeus::xhistory_manager>;
-  history_manager_ptr hist = xeus::make_in_memory_history_manager();
+    using history_manager_ptr = std::unique_ptr<xeus::xhistory_manager>;
+    history_manager_ptr hist = xeus::make_in_memory_history_manager();
   
-  if (!file_name.empty())
-  {
-    xeus::xconfiguration config = xeus::load_configuration(file_name);
+    if (!file_name.empty())
+        {
+            xeus::xconfiguration config = xeus::load_configuration(file_name);
 
-    xeus::xkernel kernel(config,
-                         xeus::get_user_name(),
-                         std::move(interpreter),
-                         std::move(hist),
-                         xeus::make_console_logger(xeus::xlogger::msg_type,
-                                                   xeus::make_file_logger(xeus::xlogger::content, "xeus.log")),
-                         xeus::make_xserver_shell_main,
-                         xclasp::make_clasp_debugger);
-    std::clog <<
-      "Starting xeus-clasp kernel...\n\n"
-      "If you want to connect to this kernel from another client, you can use"
-      " the " + file_name + " file."
-      << std::endl;
+            xeus::xkernel kernel(config,
+                                 xeus::get_user_name(),
+                                 std::move(interpreter),
+                                 std::move(hist),
+                                 xeus::make_console_logger(xeus::xlogger::msg_type,
+                                                           xeus::make_file_logger(xeus::xlogger::content, "xeus.log")),
+                                 xeus::make_xserver_shell_main,
+                                 xclasp::make_clasp_debugger);
+            std::clog <<
+                "Starting xeus-clasp kernel...\n\n"
+                "If you want to connect to this kernel from another client, you can use"
+                " the " + file_name + " file."
+                      << std::endl;
 
-    kernel.start();                       
-  }
-  else
-  {
-    std::clog << "File name is empty";
-    xeus::xkernel kernel(xeus::get_user_name(),
-                         std::move(interpreter),
-                         std::move(hist),
-                         nullptr,
-                         xeus::make_xserver_shell_main,
-                         xclasp::make_clasp_debugger);
-    const auto& config = kernel.get_config();
-    std::clog <<
-      "Starting xeus-clasp kernel...\n\n"
-      "If you want to connect to this kernel from another client, just copy"
-      " and paste the following content inside of a `kernel.json` file. And then run for example:\n\n"
-      "# jupyter console --existing kernel.json\n\n"
-      "kernel.json\n```\n{\n"
-      "    \"transport\": \"" + config.m_transport + "\",\n"
-      "    \"ip\": \"" + config.m_ip + "\",\n"
-      "    \"control_port\": " + config.m_control_port + ",\n"
-      "    \"shell_port\": " + config.m_shell_port + ",\n"
-      "    \"stdin_port\": " + config.m_stdin_port + ",\n"
-      "    \"iopub_port\": " + config.m_iopub_port + ",\n"
-      "    \"hb_port\": " + config.m_hb_port + ",\n"
-      "    \"signature_scheme\": \"" + config.m_signature_scheme + "\",\n"
-      "    \"key\": \"" + config.m_key + "\"\n"
-      "}\n```\n";
+            kernel.start();                       
+        }
+    else
+        {
+            std::clog << "File name is empty";
+            xeus::xkernel kernel(xeus::get_user_name(),
+                                 std::move(interpreter),
+                                 std::move(hist),
+                                 nullptr,
+                                 xeus::make_xserver_shell_main,
+                                 xclasp::make_clasp_debugger);
+            const auto& config = kernel.get_config();
+            std::clog <<
+                "Starting xeus-clasp kernel...\n\n"
+                "If you want to connect to this kernel from another client, just copy"
+                " and paste the following content inside of a `kernel.json` file. And then run for example:\n\n"
+                "# jupyter console --existing kernel.json\n\n"
+                "kernel.json\n```\n{\n"
+                "    \"transport\": \"" + config.m_transport + "\",\n"
+                "    \"ip\": \"" + config.m_ip + "\",\n"
+                "    \"control_port\": " + config.m_control_port + ",\n"
+                "    \"shell_port\": " + config.m_shell_port + ",\n"
+                "    \"stdin_port\": " + config.m_stdin_port + ",\n"
+                "    \"iopub_port\": " + config.m_iopub_port + ",\n"
+                "    \"hb_port\": " + config.m_hb_port + ",\n"
+                "    \"signature_scheme\": \"" + config.m_signature_scheme + "\",\n"
+                "    \"key\": \"" + config.m_key + "\"\n"
+                "}\n```\n";
 
-    kernel.start();
-  }
+            kernel.start();
+        }
 
-  return 0;
+    return 0;
 }
+   
+}
+
+void startup()
+{
+    printf("%s:%d Starting up\n", __FILE__, __LINE__ );
+    using namespace clbind;
+    package("XCLASpP") [
+                        def("start-interpreter",&start_interpreter)
+		   ];
+}
+
+CLASP_REGISTER_STARTUP(startup);
+
---
+++ src/xclasp.lisp
@@ -0,0 +1,3 @@
+;;; Load the xclasp library
+(load "/Users/meister/Development/xeus-clasp/build/libxclasp.dylib")
+;;; Then use (xclasp:start-interpreter) to start the interpreter
